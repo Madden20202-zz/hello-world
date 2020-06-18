55751079a63815b96551caa4d32d880f4dcfee74
Austin <business.mocksock@gmail.com>
2020-06-18 14:35:46 -0700

    Variable Types
    Learned the variable types and displays them. Currently trying to find a way to space the text out.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-/*
-iostream is the header file library which allows
-for the dev to work with input and output objects
-*/
+/* iostream is the header file library which allows
+for the dev to work with input and output objects */
 #include <iostream>
 
 // allows use of names for standard objects and variables in the standard library
@@ -9,10 +7,36 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World! \n";
-    // \n adds a ine break, two (\n\n) will make a blank line
+    // Let's learn about Variables!
 
-    cout << "I am learning C++";
+    //int stands for Integer, which stores whole numbers without decimals
+    int a = 10;
+
+    cout << a;
+
+    //float is used for storing floating point numbers, that have decimals
+    float pi = 3.14;
+
+    cout << pi;
+
+    //char stands for Characters and stores single letters
+    char middleInitial = 'k';
+
+    cout << middleInitial;
+
+    //string combines an array of chars into full sentences
+    string alert = "This is a basic test!";
+
+    cout << alert;
+
+    //bool stands for Boolean, which can only store true or false values
+    bool playerTurn = true;
+
+    if(playerTurn = true) {
+        cout << "It is your turn!";
+    } else {
+        cout << "An error has occured, please check the code";
+    }
 
     return 0;
 }
