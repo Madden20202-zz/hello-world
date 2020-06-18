621e26cac888e44b5b19bcab957e3399dc56cfe2
Austin <business.mocksock@gmail.com>
2020-06-18 14:12:31 -0700

    Learning Comments
    Learned how to do inline comments

--- main.cpp
+++ main.cpp
@@ -4,17 +4,13 @@ for the dev to work with input and output objects
 */
 #include <iostream>
 
-/*
-allows use of names for standard objects and variables in the standard library
-*/
+// allows use of names for standard objects and variables in the standard library
 using namespace std;
 
 int main()
 {
     cout << "Hello World! \n";
-    /*
-    \n adds a ine break, two (\n\n) will make a blank line
-    */
+    // \n adds a ine break, two (\n\n) will make a blank line
 
     cout << "I am learning C++";
 
