67fb600ab92d67bce2295e8425cbeffc01346182
Austin <business.mocksock@gmail.com>
2020-06-17 19:56:29 -0700

    Notes
    Basic notes were taken about what was going on with the top portion of code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
+/*
+iostream is the header file library which allows
+for the dev to work with input and output objects
+*/
 #include <iostream>
 
+/*
+allows use of names for standard objects and variables in the standard library
+*/
 using namespace std;
 
 int main()
