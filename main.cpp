36f507b2d04f1d9843bbf1b56f6f9ff504734166
Austin <business.mocksock@gmail.com>
2020-06-20 14:50:16 -0700

    Constants
    Learned about constants and how they are used!

--- main.cpp
+++ main.cpp
@@ -7,31 +7,16 @@ using namespace std;
 
 int main()
 {
-    // Let's learn about Variables!
+   // Constants
 
-    // Identifiers are unique names for variables
-    // all variables MUST have a unique identifier
-    // this is helpful for calling on your variable later
+   // Constants allow a coder to make a variable thats value can not be changed
 
-    // There are naming conventions
-    // Simply put, try to describe what it is
+   // Certain rules should be followed
+   // Make sure that it is a variable that will not change at all i.e. pi
 
-    int age = 32; // this is a good identifier, since it describes in one word what it is for
-    string nameForChild = "Maggie"; // this is also good, since it makes it clear that the varibale is a name for a child
+   const string favoriteFood = "Pizza";
 
-    int m = 32; // unless it is clearly stated why it is called m, such as for an equation, this is a poor identifier
-    string a = "Maggie"; // this is also an unclear identifier, because what is this used for?
+   cout << "My favorite food is " << favoriteFood;
 
-    cout << age << "\n" << nameForChild << "\n" << m << "\n" << a;
-
-    return 0; //Though this is assumed, DO NOT FORGET!!
-
-    // Here are the basic rules to making identifiers
-    /*
-        Names can contain letters, underscores, and digits
-        Names must start with a letter or underscore
-        Names are case sensitive, so var and Var are two different variables
-        Names may NOT contain whitespace or special characters like ! @ # $
-        Reserved words can not be used, such as init or main
-    */
+    return 0;
 }
