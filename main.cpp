1dd6da905cc9c9dc0bcc1fe66899fc4d9466cbda
Austin <business.mocksock@gmail.com>
2020-06-22 16:29:00 -0700

    Char and ASCII
    Explored the char data type and briefly explained ASCII (pronounced as-key)

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-   // Data Tpes
+   // Data Types
 
    /*
     C++, like C, can perform memory management!
@@ -18,7 +18,7 @@ int main()
    int age = 25; //Stores whole numbers, using 4 bytes
    float detailedAge = 25.67; //Stores fractional numbers up to seven decimal spaces, using 4 bytes
    double pi = 3.14159265358979; // Stores fractional numbers up to fifteen decimal spaces, using 4 bytes
-   bool adult = false; // Stores true or false (1-0) values, using 1 bytes
+   bool adult = true; // Stores true or false (1-0) values, using 1 bytes
    char middleInitial = 'A'; // Stores single characters, letters, or numbers using 1 byte
 
    cout << age << "\n";
@@ -27,22 +27,12 @@ int main()
    cout << adult << "\n";
    cout << middleInitial << "\n";
 
-   // Boolean Data Types
+   // Char and String
 
    /*
-    Boolean data types only use two values, 1 (true) or 0 (false).
-    This means that ultimately, anything that has this value must
-    be either true or false
+    Char is used to store single characters using single quotations ('').
+    ASCII values may also be used to represent different letters or symbols, such as ! @ $ # or %
    */
-
-   if (adult = true){
-    cout << "You are a fully grown adult!";
-    return 0;
-   } else {
-    cout << "Given time, you will become an adult!";
-    adult = true;
-    return 0;
-   }
 
    return 0;
 }
