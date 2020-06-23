2af38e3724d868456e596974ddc3bef6d49c51b3
Austin <business.mocksock@gmail.com>
2020-06-23 15:45:02 -0700

    Strings
    Recapped the basics of what a string variable is

--- main.cpp
+++ main.cpp
@@ -9,31 +9,20 @@ using namespace std;
 
 int main()
 {
-   // Operators
+   // Strings
 
    /*
-    What is an Operator?
-    An operator is anything that allows values or variables to be changed
-    You can think of it being the PEMDAS operations, though the validity of this is not confirmed
+    It is time to explore strings!
+
+    Strings are used to store text, and
+    are surrounded by double quotations ("")
    */
 
-   // Logical Operators
+   string greeting = "Hello!";
 
-   //Logical Operators checks the values and gives responses via boolean operators
+   cout << greeting << "\n";
 
-   /*
-    && = logical and, which will only work when both values are the same
-    || = logical or, which works if either variable is valid
-    ! = logical not, which works if the value is not the value looked for
-   */
 
-   int age = 22, creditCards = 3;
-
-   cout << (age < 18 && creditCards < 1) << "\n";// returns 0 since this is false
-
-   cout << (age < 18 || creditCards < 5) << "\n"; // returns 1 since the creditCards value is less than 5
-
-   cout << !(age << 12 && creditCards > 10); // returns 1 since the statement is false but the not turns it true
 
    return 0;
 }
